cbd31a4c89a36b57385864b39b82f4500c305c02
koala999 <koala999@aliyun.com>
2022-03-15 22:30:47 +0800

    init commit

---
+++ src/KgFraming.cpp
@@ -0,0 +1,106 @@
+﻿#include "KgFraming.h"
+#include "KcSampled1d.h"
+#include "KcSampled2d.h"
+#include "KtuBitwise.h"
+#include "KtuMath.h"
+#include <assert.h>
+
+
+KgFraming::KgFraming(kReal sampleRate, kIndex channels)
+    : shift_(0.01)
+    , length_(0.025)
+    , roundPower2_(true)
+{
+	buf_ = std::make_unique<KcSampled1d>(1 / sampleRate, 0, channels);
+}
+
+
+void KgFraming::reset(kReal sampleRate, kIndex channels)
+{
+	buf_->reset(1 / sampleRate, channels);
+}
+
+
+void KgFraming::flush(KcSampled2d& out)
+{
+	if (buf_->empty()) {
+		out.clear();
+		return;
+	}
+
+	auto samplesPerFrame = this->samplesPerFrame();
+	out.resize(1, samplesPerFrame, buf_->channels());
+	out.reset(0, buf_->sampling().low() + length_ / 2, shift_);
+	out.reset(1, buf_->sampling().low(), buf_->step(0));
+
+	kReal* buf = out.at(0);
+	buf_->getSamples(0, buf, buf_->count());
+	::memset(buf + buf_->count(), 0, sizeof(kReal) * (samplesPerFrame - buf_->count()));
+}
+
+
+void KgFraming::process(const KcSampled1d& in, KcSampled2d& out)
+{
+    assert(KtuMath<kReal>::almostEqualRel(buf_->samplingRate(), in.samplingRate()));
+	assert(in.channels() == buf_->channels());
+
+    // 计算输入信号能分多少帧
+	buf_->append(in);
+	auto frames = numFrames(buf_->count());
+
+    // 预留输出空间
+	auto frameSize = this->frameSize();
+	auto samplesPerFrame = this->samplesPerFrame();
+	out.resize(frames, samplesPerFrame, buf_->channels());
+	out.reset(0, buf_->sampling().low() + length_ / 2, shift_);
+	out.reset(1, buf_->sampling().low(), buf_->step(0));
+
+	// 输出分帧
+	auto shiftSize = this->shiftSize();
+	kIndex frameFirst(0);
+	for (kIndex i = 0; i < frames; i++) {
+		assert(frameFirst + frameSize < in.count());
+		kReal* buf = out.at(i);
+		in.getSamples(frameFirst, buf, frameSize);
+
+		if (samplesPerFrame != frameSize)  // 补零
+			::memset(buf + frameSize, 0, sizeof(kReal) * (samplesPerFrame - frameSize));
+
+		frameFirst += shiftSize;
+	}
+
+	// 修正缓存
+	assert(frameFirst + frameSize > buf_->count());
+	buf_->popFront(frameFirst);
+	assert(buf_->count() < frameSize);
+}
+
+
+
+kIndex KgFraming::frameSize() const
+{	
+	return buf_->sampling().countLength(length_); 
+}
+
+
+kIndex KgFraming::shiftSize() const
+{
+	return buf_->sampling().countLength(shift_); 
+}
+
+
+kIndex KgFraming::numFrames(kIndex samples)
+{
+	auto frameSize = this->frameSize();
+	if (samples < frameSize)
+		return 0;
+
+	return 1 + (samples - frameSize) / shiftSize();
+}
+
+
+kIndex KgFraming::samplesPerFrame() const
+{
+	auto frameSize = this->frameSize();
+	return roundPower2_ ? KtuBitwise<kIndex>::ceilPower2(frameSize) : frameSize;
+}
---
+++ src/KgFraming.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include "kDsp.h"
+#include <memory>
+
+class KcSampled1d;
+class KcSampled2d;
+
+
+// 对一维信号作分帧处理
+class KgFraming
+{
+public:
+	KgFraming(kReal sampleRate, kIndex channels = 1);
+
+	void reset(kReal sampleRate, kIndex channels = 1);
+	void flush(KcSampled2d& out);
+
+	// 输出的分帧结果可能有多个，所以用二维信号out表示
+	void process(const KcSampled1d& in, KcSampled2d& out);
+
+	// 给定采样点数目，计算可分帧数
+	kIndex numFrames(kIndex samples);
+
+	kIndex frameSize() const; // 每帧的采样点数目
+	kIndex shiftSize() const; // 每次偏移的采样点数目
+
+	// roundPower2_为false时，返回值等于frameSize，否则等于ceilPower2(frameSize)
+	kIndex samplesPerFrame() const;
+
+private:
+	kReal shift_; // in second
+	kReal length_; // in second
+
+	// If true, round window size to power of two by zero-padding input to FFT.
+	bool  roundPower2_; 
+
+	// If true, allow the input waveform to have a higher frequency than sampleRate_
+	//TODO: bool  allowDownsample_; 
+
+	std::unique_ptr<KcSampled1d> buf_; // 待处理数据，长度小于length_
+};
+
