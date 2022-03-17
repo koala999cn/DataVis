cd4e1044bfa2176df06bb57b900d7efd8abea8f0
koala999 <koala999@aliyun.com>
2022-03-17 13:03:06 +0800

    init commit

---
+++ src/dsp/KgMultiply.cpp
@@ -0,0 +1,21 @@
+﻿#include "KgMultiply.h"
+#include "KcSampled1d.h"
+
+
+KgMultiply::KgMultiply(kIndex dim, kIndex channels)
+{
+	sig_ = std::make_unique<KcSampled1d>();
+	sig_->resize(dim, channels);
+}
+
+
+void KgMultiply::process(const KcSampled1d& in, KcSampled1d& out)
+{
+	assert(in.count() == sig_->count());
+}
+
+
+void KgMultiply::porcess(KcSampled1d& inout)
+{
+	assert(inout.count() == sig_->count());
+}
---
+++ src/dsp/KgMultiply.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include <memory>
+#include "kDsp.h"
+
+class KcSampled1d;
+
+
+// 信号乘法，要求两个信号尺度一致
+
+class KgMultiply
+{
+public:
+	KgMultiply(kIndex dim, kIndex channels = 1);
+
+	void process(const KcSampled1d& in, KcSampled1d& out);
+	void porcess(KcSampled1d& inout);
+
+private:
+	std::unique_ptr<KcSampled1d> sig_; // 与输入相乘的信号
+};
+
