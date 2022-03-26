75204287060b4fbfdaba6b2cf8c113bb63c4a50d
koala999 <koala999@aliyun.com>
2022-03-26 13:52:32 +0800

    实现连续、散点、采样3类数据的区分

--- src/dsp/KvData.h
+++ src/dsp/KvData.h
@@ -16,8 +16,10 @@ public:
 	// 数据的维度
 	virtual kIndex dim() const = 0; 
 
-	// 包含的数据总数
+	// 包含的数据总数，返回inf对应连续数据
 	virtual kIndex count() const = 0;
+
+	constexpr static kReal k_inf_count = std::numeric_limits<kReal>::infinity();
 
 	// 支持多通道数据，返回通道数目
 	virtual kIndex channels() const = 0;
@@ -35,9 +37,27 @@ public:
 	virtual kRange range(kIndex axis) const = 0;
 
 	// 返回axis维度的步进，即dx, dy, dz...
-	// 返回=0表示步进未知，返回inf表示步进非均匀
+	// 返回0表示步进未知，对应于连续数据
+	// 返回inf表示步进非均匀，对应于散点数据
 	virtual kReal step(kIndex axis) const = 0;
 
 	constexpr static kReal k_unknown_step = 0;
 	constexpr static kReal k_nonuniform_step = std::numeric_limits<kReal>::infinity();
+
+
+	// 是否连续数据？
+	bool isContinued() const {
+		return count() == k_inf_count;
+	}
+
+	// 是否散点数据？
+	bool isScattered() const {
+		return step(0) == k_nonuniform_step;
+	}
+
+	// 是否采样数据
+	bool isSampled() const {
+		return count() != k_inf_count && step(0) != k_nonuniform_step;
+	}
+
 };
