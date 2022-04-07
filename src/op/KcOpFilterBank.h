801053140d126935b41c9f3647d14d7c927d1c66
koala999 <koala999@aliyun.com>
2022-04-07 18:47:55 +0800

    clean code

--- src/op/KcOpFilterBank.h
+++ src/op/KcOpFilterBank.h
@@ -11,8 +11,6 @@ public:
 
 	bool isStream() const override { return false; }
 
-	//kIndex dim() const override;
-
 	kRange range(kIndex axis) const override;
 
 	kReal step(kIndex axis) const override;
@@ -30,12 +28,7 @@ private:
 
 	void processNaive_(const kReal* in, unsigned len, kReal* out) override;
 
-
-	//std::shared_ptr<KvData> processImpl_(std::shared_ptr<KvData> data) override;
-
 	void syncParent() override;
-
-	//std::shared_ptr<KvData> process2d_(std::shared_ptr<KvData> data);
 
 private:
 	std::unique_ptr<KgFilterBank> fbank_;
