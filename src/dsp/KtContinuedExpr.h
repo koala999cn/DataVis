200faab271f4e77099b034dd63c85b2412fdc956
koala999 <koala999@aliyun.com>
2022-04-11 12:33:29 +0800

    refactor: 重新实现基于exprtk的连续数据类

---
+++ src/dsp/KtContinuedExpr.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+#include <memory>
+#include <stdarg.h>
+#include "KtContinued.h"
+
+
+// 基于exprtk的连续数据实现
+
+class KvExprtk;
+
+namespace kPrivate
+{
+    struct KpExprtkHelper_ {
+        KpExprtkHelper_(std::shared_ptr<KvExprtk> expr)
+            : expr_(expr) {}
+
+        kReal operator()(kReal x, ...) const {
+            va_list args;
+            va_start(args, x);
+            kReal r = expr_->value(x, args);
+            va_end(args);
+            return r;
+        }
+
+    private:
+        std::shared_ptr<KvExprtk> expr_;
+    };
+};
+
+template<int DIM>
+class KtContinuedExpr : public KtContinued<kPrivate::KpExprtkHelper_, DIM>
+{
+public:
+    using super_ = KtContinued<kPrivate::KpExprtkHelper_, DIM>;
+
+    KtContinuedExpr(std::shared_ptr<KvExprtk> expr)
+        : super_(expr) {}
+};
+
