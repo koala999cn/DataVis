8ed314913cebf1bb8441de3a8c07315f795c56d0
koala999 <koala999@aliyun.com>
2022-04-13 21:49:35 +0800

    add: 新增实现采样器

---
+++ src/dsp/KtSampler.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include <memory>
+#include "KtSampled.h"
+
+
+// 对连续数据的采样器
+
+template<int DIM>
+class KtSampler : public KtSampled<DIM>
+{
+public:
+	KtSampler(std::shared_ptr<KvData>& cont) 
+		: internal_(cont) {}
+
+
+	kReal value(kIndex idx[], kIndex channel) const final {
+		kReal pt[DIM];
+		for (kIndex i = 0; i < DIM; i++)
+			pt[i] = indexToValue(idx[i]);
+
+		return value(pt, channel);
+	}
+
+
+	kReal value(kReal pt[], kIndex channel) const final {
+		return internal_->value(pt, channel);
+	}
+
+private:
+	const std::shared_ptr<KvData> internal_;
+};
