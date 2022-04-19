dc6c1e9fb7536d119843889c4c6d047be5ed8c4d
koala999 <koala999@aliyun.com>
2022-04-19 12:39:48 +0800

    add: 外插支持类

---
+++ src/dsp/KuExtrapolate.cpp
@@ -0,0 +1,36 @@
+﻿#include "KuExtrapolate.h"
+#include <cmath>
+
+
+double KuExtrapolate::nearest(double low, double high, double x) 
+{
+    return x > high ? high : low;
+}
+
+
+double KuExtrapolate::mirror(double low, double high, double x) 
+{
+    auto length = high - low;
+    if (x > high) {
+        auto ratio = std::fmod(x - high, length);
+        return high - ratio * length;
+    }
+    else {
+        auto ratio = std::fmod(low - x, length);
+        return low + ratio * length;
+    }
+}
+
+
+double KuExtrapolate::period(double low, double high, double x) 
+{
+    auto length = high - low;
+    if (x > high) {
+        auto ratio = std::fmod(x - high, length);
+        return low + ratio * length;
+    }
+    else {
+        auto ratio = std::fmod(low - x, length);
+        return high - ratio * length;
+    }
+}
---
+++ src/dsp/KuExtrapolate.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+
+class KuExtrapolate
+{
+public:
+
+    /// 几种对超范围坐标x的外插
+
+    static double nearest(double low, double high, double x);
+
+    static double mirror(double low, double high, double x);
+
+    static double period(double low, double high, double x);
+
+private:
+	KuExtrapolate() = delete;
+};
