43ccfafbcca4e526ff9a94c4f440299a9ee7d8cf
koala999cn <koala999@aliyun.com>
2022-09-20 09:35:10 +0800

    refactor: 优化trackball，取消坐标转换

--- src/plot/KcPlotApplet_.cpp
+++ src/plot/KcPlotApplet_.cpp
@@ -55,14 +55,6 @@ namespace kPrivate
     }
 
 
-    void KcPlotApplet_::mapToViewport_(int& x, int& y)
-    {
-        // set x/y relative to the viewport
-        x -= viewport_()->x();
-        y -= openglContext()->framebuffer()->height() - 1 - (viewport_()->y() + viewport_()->height() - 1);
-    }
-
-
     void KcPlotApplet_::initEvent()
     {
         auto r = rendering()->as<vl::Rendering>();
@@ -97,34 +89,23 @@ namespace kPrivate
 
         VL_CHECK(openglContext()->framebuffer());
 
-        mapToViewport_(x, y);
-
-        // check that the click is in the viewport
-        int w = viewport_()->width();
-        int h = viewport_()->height();
-
-        if (x < 0 || y < 0 || x >= w || y >= h)
-            return;
-
-        posX_ = x, posY_ = y;
+        if (viewport_()->isPointInside(x, y, openglContext()->framebuffer()->height()))
+            posX_ = x, posY_ = y;
     }
 
 
     void KcPlotApplet_::mouseUpEvent(vl::EMouseButton btn, int x, int y)
     {
         btn_ = vl::NoButton;
-        posX_ = x, posY_ = y;
     }
 
 
     void KcPlotApplet_::mouseMoveEvent(int x, int y)
     {
-        VL_CHECK(openglContext()->framebuffer())
+        VL_CHECK(openglContext()->framebuffer());
 
-            mapToViewport_(x, y);
-
-        auto dx = posX_ - x;
-        auto dy = -(posY_ - y);
+        auto dx = x - posX_;
+        auto dy = y - posY_;
 
         if (btn_ == vl::LeftButton) {
             doRotate_(dx, dy);
@@ -141,10 +122,11 @@ namespace kPrivate
 
     void KcPlotApplet_::doRotate_(int dx, int dy)
     {
-        double relx = 90 * dx * rotateSpeed_ / viewport_()->width();
-        double relyz = 90 * dy * rotateSpeed_ / viewport_()->height();
+        // 鼠标每移动一个viewport长度，旋转90度
+        double relx = dx * rotateSpeed_ / viewport_()->width() * 90;
+        double relyz = dy * rotateSpeed_ / viewport_()->height() * 90;
 
-        plot3d_->rotate({ relyz, -relx, -relx });
+        plot3d_->rotate({ relyz, relx, relx });
     }
 
 
@@ -154,8 +136,8 @@ namespace kPrivate
         double rely = dy * shiftSpeed_ / viewport_()->height();
 
         auto box = plot3d_->coordSystem().boundingBox();
-        auto delta = (box.upper() - box.lower()) * point3d(relx, rely, 0);
-        plot3d_->shift(-delta);
+        auto delta = (box.upper() - box.lower()) * point3d(relx, -rely, 0); // 屏幕的y轴坐标与视图的y轴坐标反向
+        plot3d_->shift(delta);
     }
 
 
