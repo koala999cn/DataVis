db587d2001aac326db0611d7cd35517308ef1950
koala999cn <koala999@aliyun.com>
2022-09-30 17:08:09 +0800

    refactor: ImGiozmo window abstract class

--- src/imapp/KvImGuizmoWindow.cpp
+++ src/imapp/KvImGuizmoWindow.cpp
@@ -1,141 +1,25 @@
 #include "KvImGuizmoWindow.h"
 #include "imgui.h"
 #include "ImGuizmo.h"
-#include "KtCamera.h" // TODO:
+#include "KtCamera.h" 
 
 
-int gizmoCount = 3;
-float camDistance = 8.f;
-static ImGuizmo::OPERATION mCurrentGizmoOperation(ImGuizmo::TRANSLATE);
-
-float objectMatrix[4][16] = {
-  { 1.f, 0.f, 0.f, 0.f,
-    0.f, 1.f, 0.f, 0.f,
-    0.f, 0.f, 1.f, 0.f,
-    0.f, 0.f, 0.f, 1.f },
-
-  { 1.f, 0.f, 0.f, 0.f,
-  0.f, 1.f, 0.f, 0.f,
-  0.f, 0.f, 1.f, 0.f,
-  2.f, 0.f, 0.f, 1.f },
-
-  { 1.f, 0.f, 0.f, 0.f,
-  0.f, 1.f, 0.f, 0.f,
-  0.f, 0.f, 1.f, 0.f,
-  2.f, 0.f, 2.f, 1.f },
-
-  { 1.f, 0.f, 0.f, 0.f,
-  0.f, 1.f, 0.f, 0.f,
-  0.f, 0.f, 1.f, 0.f,
-  0.f, 0.f, 2.f, 1.f }
-};
-
-static const float identityMatrix[16] =
-{ 1.f, 0.f, 0.f, 0.f,
-    0.f, 1.f, 0.f, 0.f,
-    0.f, 0.f, 1.f, 0.f,
-    0.f, 0.f, 0.f, 1.f };
-
-static ImGuizmo::MODE mCurrentGizmoMode(ImGuizmo::LOCAL);
-static bool useSnap = false;
-static float snap[3] = { 1.f, 1.f, 1.f };
-static float bounds[] = { -0.5f, -0.5f, -0.5f, 0.5f, 0.5f, 0.5f };
-static float boundsSnap[] = { 0.1f, 0.1f, 0.1f };
-static bool boundSizing = false;
-static bool boundSizingSnap = false;
-
-float cameraView[16] =
-{ 1.f, 0.f, 0.f, 0.f,
-  0.f, 1.f, 0.f, 0.f,
-  0.f, 0.f, 1.f, 0.f,
-  0.f, 0.f, 0.f, 1.f };
-
-void Frustum(float left, float right, float bottom, float top, float znear, float zfar, float* m16)
+KvImGuizmoWindow::KvImGuizmoWindow(const std::string_view& name)
+    : KvImWindow(name)
 {
-    float temp, temp2, temp3, temp4;
-    temp = 2.0f * znear;
-    temp2 = right - left;
-    temp3 = top - bottom;
-    temp4 = zfar - znear;
-    m16[0] = temp / temp2;
-    m16[1] = 0.0;
-    m16[2] = 0.0;
-    m16[3] = 0.0;
-    m16[4] = 0.0;
-    m16[5] = temp / temp3;
-    m16[6] = 0.0;
-    m16[7] = 0.0;
-    m16[8] = (right + left) / temp2;
-    m16[9] = (top + bottom) / temp3;
-    m16[10] = (-zfar - znear) / temp4;
-    m16[11] = -1.0f;
-    m16[12] = 0.0;
-    m16[13] = 0.0;
-    m16[14] = (-temp * zfar) / temp4;
-    m16[15] = 0.0;
+    KtCamera<float> cam;
+
+    vec3f eye{ 5, 5, 5 };
+    vec3f at{};
+    vec3f up{ 0, 1, 0 };
+    cam.lookAt(eye, at, up);
+    viewMat_ = cam.viewMatrix().getTransposed(); // ImGuizmo使用的矩阵与KtCamera使用的为转置关系
+
+    ImGuiIO& io = ImGui::GetIO();
+    cam.projectPerspective(45, io.DisplaySize.x / io.DisplaySize.y, 5, 100.f);
+    projMat_ = cam.projMatrix().getTransposed();
 }
 
-void Perspective(float fovyInDegrees, float aspectRatio, float znear, float zfar, float* m16)
-{
-    float ymax, xmax;
-    ymax = znear * tanf(fovyInDegrees * 3.141592f / 180.0f);
-    xmax = ymax * aspectRatio;
-    Frustum(-xmax, xmax, -ymax, ymax, znear, zfar, m16);
-}
-
-
-void Cross(const float* a, const float* b, float* r)
-{
-    r[0] = a[1] * b[2] - a[2] * b[1];
-    r[1] = a[2] * b[0] - a[0] * b[2];
-    r[2] = a[0] * b[1] - a[1] * b[0];
-}
-
-float Dot(const float* a, const float* b)
-{
-    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
-}
-
-void Normalize(const float* a, float* r)
-{
-    float il = 1.f / (sqrtf(Dot(a, a)) + FLT_EPSILON);
-    r[0] = a[0] * il;
-    r[1] = a[1] * il;
-    r[2] = a[2] * il;
-}
-
-
-void LookAt(const float* eye, const float* at, const float* up, float* m16)
-{
-    float X[3], Y[3], Z[3], tmp[3];
-
-    tmp[0] = eye[0] - at[0];
-    tmp[1] = eye[1] - at[1];
-    tmp[2] = eye[2] - at[2];
-    Normalize(tmp, Z);
-    Normalize(up, Y);
-    Cross(Y, Z, tmp);
-    Normalize(tmp, X);
-    Cross(Z, X, tmp);
-    Normalize(tmp, Y);
-
-    m16[0] = X[0];
-    m16[1] = Y[0];
-    m16[2] = Z[0];
-    m16[3] = 0.0f;
-    m16[4] = X[1];
-    m16[5] = Y[1];
-    m16[6] = Z[1];
-    m16[7] = 0.0f;
-    m16[8] = X[2];
-    m16[9] = Y[2];
-    m16[10] = Z[2];
-    m16[11] = 0.0f;
-    m16[12] = -Dot(X, eye);
-    m16[13] = -Dot(Y, eye);
-    m16[14] = -Dot(Z, eye);
-    m16[15] = 1.0f;
-}
 
 void KvImGuizmoWindow::update()
 {
@@ -144,46 +28,12 @@ void KvImGuizmoWindow::update()
         ImVec2{ maxSize_[0], maxSize_[1] }
     );
 
-    float cameraProjection[16];
-    float fov = 27.f;
-    ImGuiIO& io = ImGui::GetIO();
-    Perspective(fov, io.DisplaySize.x / io.DisplaySize.y, 0.1f, 100.f, cameraProjection);
-
-    vec3f eye{ 0, 10, 0 };
-    vec3f at{};
-    vec3f up{ 0, 1, 0 };
-   // LookAt(eye.data(), at.data(), up.data(), cameraView);
-    KtCamera<float> cam;
-    cam.projectPerspective(fov, io.DisplaySize.x / io.DisplaySize.y, 0.1f, 100.f);
-    auto projMat = cam.projMatrix().getTransposed();
-
-    cam.lookAt(eye, at, up);
-    auto viewMat = cam.viewMatrix();
-
-    float* matrix = objectMatrix[0];
-
-    float viewManipulateRight = io.DisplaySize.x;
-    float viewManipulateTop = 0;
-
-    if (ImGui::Begin(name().c_str(), &visible_, 
-        noMove_ ? flags() | ImGuiWindowFlags_NoMove : flags())) {
-
-        assert(visible_);
+    auto f = noMove_ ? flags() | ImGuiWindowFlags_NoMove : flags();
+    if (ImGui::Begin(name().c_str(), &visible_, f) && visible()) {
 
         ImGuizmo::SetDrawlist();
-        float windowWidth = ImGui::GetWindowWidth();
-        float windowHeight = ImGui::GetWindowHeight();
-        ImGuizmo::SetRect(ImGui::GetWindowPos().x, ImGui::GetWindowPos().y, windowWidth, windowHeight);
-        viewManipulateRight = ImGui::GetWindowPos().x + windowWidth;
-        viewManipulateTop = ImGui::GetWindowPos().y;
-        //ImGuiWindow* window = ImGui::GetWindowViewport();
-
-        ImGuizmo::DrawGrid(cameraView, cameraProjection, identityMatrix, 100.f);
-        ImGuizmo::DrawCubes(cameraView, cameraProjection, &objectMatrix[0][0], gizmoCount);
-        //ImGuizmo::Manipulate(cameraView, cameraProjection, 
-        //    mCurrentGizmoOperation, mCurrentGizmoMode, matrix, NULL, useSnap ? &snap[0] : NULL, boundSizing ? bounds : NULL, boundSizingSnap ? boundsSnap : NULL);
-
-        ImGuizmo::ViewManipulate(cameraView, camDistance, ImVec2(viewManipulateRight - 128, viewManipulateTop), ImVec2(128, 128), 0x10101010);
+        ImGuizmo::SetRect(ImGui::GetWindowPos().x, ImGui::GetWindowPos().y, 
+            ImGui::GetWindowWidth(), ImGui::GetWindowHeight());
 
         updateImpl_();
 
@@ -191,4 +41,52 @@ void KvImGuizmoWindow::update()
     }
 
     ImGui::End();
+}
+
+
+void KvImGuizmoWindow::updateImpl_()
+{
+    int gizmoCount = 3;
+    float camDistance = 8.f;
+    static ImGuizmo::OPERATION mCurrentGizmoOperation(ImGuizmo::TRANSLATE);
+
+    float objectMatrix[16] = {
+        1.f, 0.f, 0.f, 0.f,
+        0.f, 1.f, 0.f, 0.f,
+        0.f, 0.f, 1.f, 0.f,
+        0.f, 0.f, 0.f, 1.f 
+    };
+
+    static const float identityMatrix[16] =
+    { 1.f, 0.f, 0.f, 0.f,
+        0.f, 1.f, 0.f, 0.f,
+        0.f, 0.f, 1.f, 0.f,
+        0.f, 0.f, 0.f, 1.f };
+
+    static ImGuizmo::MODE mCurrentGizmoMode(ImGuizmo::LOCAL);
+    static bool useSnap = false;
+    static float snap[3] = { 1.f, 1.f, 1.f };
+    static float bounds[] = { -0.5f, -0.5f, -0.5f, 0.5f, 0.5f, 0.5f };
+    static float boundsSnap[] = { 0.1f, 0.1f, 0.1f };
+    static bool boundSizing = false;
+    static bool boundSizingSnap = false;
+
+    ImGuiIO& io = ImGui::GetIO();
+    float viewManipulateRight = io.DisplaySize.x;
+    float viewManipulateTop = 0;
+
+    viewManipulateRight = ImGui::GetWindowPos().x + ImGui::GetWindowWidth();
+    viewManipulateTop = ImGui::GetWindowPos().y;
+
+    ImGuizmo::DrawGrid(viewMat_.data(), projMat_.data(), identityMatrix, 100.f);
+    ImGuizmo::DrawCubes(viewMat_.data(), projMat_.data(), objectMatrix, 1);
+    //ImGuizmo::Manipulate(viewMat_.data(), projMat_.data(),
+    //   mCurrentGizmoOperation, mCurrentGizmoMode, objectMatrix, NULL, useSnap ? &snap[0] : NULL, boundSizing ? bounds : NULL, boundSizingSnap ? boundsSnap : NULL);
+
+    ImGuizmo::ViewManipulate(viewMat_.data(), camDistance, ImVec2(viewManipulateRight - 128, viewManipulateTop), ImVec2(128, 128), 0x10101010);
+
+    ImGui::DragFloat4("row0", &viewMat_[0]);
+    ImGui::DragFloat4("row1", &viewMat_[4]);
+    ImGui::DragFloat4("row2", &viewMat_[8]);
+    ImGui::DragFloat4("row2", &viewMat_[12]);
 }
--- src/imapp/KvImGuizmoWindow.h
+++ src/imapp/KvImGuizmoWindow.h
@@ -1,16 +1,21 @@
 #pragma once
 #include "KvImWindow.h"
+#include "KtMatrix4.h"
 
 
 class KvImGuizmoWindow : public KvImWindow
 {
 public:
 
-	using KvImWindow::KvImWindow;
+	KvImGuizmoWindow(const std::string_view& name);
 
 	void update() override;
 
-	void updateImpl_() override {}
+	// 缺省显示ImGuizmo的box例子
+	void updateImpl_() override;
+
+protected:
+	mat4f viewMat_, projMat_;
 
 private:
 	bool noMove_{ false }; // 禁止用户操作trackball的时候，ImGui同时移动窗口
