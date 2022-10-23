11c7cd994fc4fcec486f39a8c6aeec584dfaf2b8
koala999 <koala999@aliyun.com>
2022-10-23 18:50:17 +0800

    fix: a trivial bug of draw line strip

--- src/imapp/KcImPaint.cpp
+++ src/imapp/KcImPaint.cpp
@@ -83,7 +83,7 @@ void KcImPaint::drawLineStrip(point_getter fn, unsigned count)
 	if (lineStyle_ == KpPen::k_solid) {
 		// TODO: 点数太大（~44k）时会crack
 		// 去除drawList->Flags的ImDrawListFlags_AntiAliasedLines标记可正常云心，但很慢
-		for (unsigned i = 1; i < count; i++)
+		for (unsigned i = 0; i < count; i++)
 			drawList->PathLineTo(world2Pos_(fn(i), true));
 		drawList->PathStroke(color_(), 0, lineWidth_); 
 	}
