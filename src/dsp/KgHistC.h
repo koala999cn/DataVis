03e6c03d94246cac4dda9b785c4db78a36e37aed
koala999cn <koala999@aliyun.com>
2022-11-08 15:20:44 +0800

    clean code

--- src/dsp/KgHistC.h
+++ src/dsp/KgHistC.h
@@ -2,7 +2,17 @@
 #include "KmBins.h"
 
 
-// 直方图计数
+// TODO: 待实现
+// 计数直方统计
+
+// 支持以下类型：
+//   count: vi = ci，为观测值的计数或频率
+//   countdensity: vi = ci / wi，计数除以bin的宽度
+//   cumcount: vi = sum(cj), 1 <= j <= i, 累积计数
+//   probability: vi = ci / N，相对概率
+//   pdf: vi = ci / (N * wi), 概率密度函数估计
+//   cdf: vi = sum(cj/N), 1 <= j <= i, 累积密度函数估计
+//
 
 class KgHistC : public KmBins
 {
