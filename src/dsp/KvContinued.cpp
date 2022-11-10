048f144caca2a88b53e850d1bf8769083f78fdf0
koala999 <koala999@aliyun.com>
2022-11-10 08:50:43 +0800

    refactor: improved the valueRange method of continued data

--- src/dsp/KvContinued.cpp
+++ src/dsp/KvContinued.cpp
@@ -1,7 +1,8 @@
 ﻿#include "KvContinued.h"
 #include "KtuMath.h"
 #include <assert.h>
-#include "KcSampler.h"
+#include "minmax.h"
+
 
 kRange KvContinued::valueRange(kIndex channel) const 
 {
@@ -10,71 +11,18 @@ kRange KvContinued::valueRange(kIndex channel) const
 		if (length(i) == 0)
 			return { 0, 0 };
 
-	std::shared_ptr<KvContinued> cont;
-	cont.reset(const_cast<KvContinued*>(this));
-	KcSampler samp(cont);
+	auto fn = [this, channel](double* x) {
+		return this->value(x, channel);
+	};
 
-	kReal omin = std::numeric_limits<kReal>::max();
-	kReal omax = std::numeric_limits<kReal>::lowest();
-	kReal tol(0.001); // 千分之一的误差
-	int maxIter(6), numIter(0);
-
-	while (numIter <= maxIter) {
-		auto r = samp.valueRange(channel);
-
-		if ((KtuMath<kReal>::almostEqualRel(omin, r.low(), tol) &&
-			KtuMath<kReal>::almostEqualRel(omax, r.high(), tol))) {
-			if (r.low() < omin) omin = r.low();
-			if (r.high() > omax) omax = r.high();
-			break;
-		}
-			
-		if (r.low() < omin) omin = r.low();
-		if (r.high() > omax) omax = r.high();
-
-		for (kIndex i = 0; i < samp.dim(); i++)
-			samp.reset(i, samp.range(i).low(), samp.step(i) / 2);
-
-		if (samp.size() > 1024 * 1024) // TODO: 满足实时性要求
-			break;
-
-		++numIter;
+	int N = std::pow(1024., 1. / dim());
+	std::vector<kReal> x0(dim()), x1(dim()), dx(dim());
+	for (unsigned i = 0; i < dim(); i++) {
+		auto r = range(i);
+		x0[i] = r.low();
+		x1[i] = r.high();
+		dx[i] = r.length() / N;
 	}
 
-	return { omin, omax }; // make compiler happy
-
-/*
-	kReal omin = std::numeric_limits<kReal>::max();
-	kReal omax = std::numeric_limits<kReal>::lowest();
-
-	kReal low = range(0).low();
-	if (std::isinf(low)) low = -1e8;
-	kReal high = range(0).high();
-	if (std::isinf(high)) high = 1e8;
-	kReal dx((high - low) / 2);
-	kReal tol(0.001); // 百分之一的误差
-	int minIter(10), maxIter(16), numIter(0);
-
-	while (true) {
-		kReal nmin = std::numeric_limits<kReal>::max();
-		kReal nmax = std::numeric_limits<kReal>::lowest();
-		for (kReal x = low; x < high; x += dx) {
-			auto val = value(&x, channel);
-			if (val > nmax)
-				nmax = val;
-			if (val < nmin)
-				nmin = val;
-		}
-
-		if ((KtuMath<kReal>::almostEqualRel(omin, nmin, tol) &&
-			KtuMath<kReal>::almostEqualRel(omax, nmax, tol) &&
-			numIter > minIter) || numIter > maxIter)
-			return { nmin, nmax };
-
-
-		omin = nmin, omax = nmax, dx *= 0.5;
-		++numIter;
-	}
-
-	return { omin, omax };*/
+	return minmax(fn, dim(), x0.data(), x1.data(), dx.data());
 }
