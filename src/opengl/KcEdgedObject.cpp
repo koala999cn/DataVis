4a9c4a0b7b58a590dbaf2648242b5c5983a7d18e
koala999cn <koala999@aliyun.com>
2023-01-09 13:23:54 +0800

    fix: 使用glPolygonOffset初步解决Z-fighting

--- src/opengl/KcEdgedObject.cpp
+++ src/opengl/KcEdgedObject.cpp
@@ -22,11 +22,20 @@ void KcEdgedObject::draw() const
     }
 
     if (edge_ && edgeShader_) {
+        if (fill_) {
+            glPolygonOffset(-1, -1);
+            glEnable(GL_POLYGON_OFFSET_FILL);
+        }
+
         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         glLineWidth(edgeWidth_);
         edgeShader_->useProgram();
         setUniforms_(edgeShader_);
         drawVbo_(); // 使用已绑定的vbo
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); // 恢复渲染状态
+
+        if (fill_) {
+            glDisable(GL_POLYGON_OFFSET_FILL);
+        }
     }
 }
