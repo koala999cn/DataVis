08c427313af2c4ea1bd13bc74512f96e83193ac3
koala999cn <koala999@aliyun.com>
2023-01-09 13:34:14 +0800

    refactor: 使用fill偏移替换edge偏移解决Z-fighting（效果更好）

--- src/opengl/KcEdgedObject.cpp
+++ src/opengl/KcEdgedObject.cpp
@@ -13,29 +13,31 @@ KcEdgedObject::KcEdgedObject(KePrimitiveType type)
 
 void KcEdgedObject::draw() const
 {
-    if (fill_) {
-        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-        super_::draw();
-    }
-    else {
-        bindVbo_();
-    }
+    bool hasEdge = edge_ && edgeShader_;
 
-    if (edge_ && edgeShader_) {
-        if (fill_) {
-            glPolygonOffset(-1, -1);
+    if (fill_) {
+        if (hasEdge) {
+            glPolygonOffset(1, 1);
             glEnable(GL_POLYGON_OFFSET_FILL);
         }
 
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        super_::draw();
+
+        if (hasEdge) {
+            glDisable(GL_POLYGON_OFFSET_FILL);
+        }
+    }
+    else if (hasEdge) {
+        bindVbo_();
+    }
+
+    if (hasEdge) {
         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         glLineWidth(edgeWidth_);
         edgeShader_->useProgram();
         setUniforms_(edgeShader_);
         drawVbo_(); // 使用已绑定的vbo
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); // 恢复渲染状态
-
-        if (fill_) {
-            glDisable(GL_POLYGON_OFFSET_FILL);
-        }
     }
 }
