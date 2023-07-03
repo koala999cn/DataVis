8c5d4e4c42dd89f660c5abcca03e9b4e7bcbbb39
koala999 <koala999@aliyun.com>
2023-07-03 09:06:41 +0800

    add: saveToFile成员方法

--- src/plot/backend/cairo/KcCairoSurfaceImage.cpp
+++ src/plot/backend/cairo/KcCairoSurfaceImage.cpp
@@ -12,3 +12,9 @@ void* KcCairoSurfaceImage::data() const
 {
 	return cairo_image_surface_get_data((cairo_surface_t*)surf_);
 }
+
+
+bool KcCairoSurfaceImage::saveToFile(const std::string_view& path) const
+{
+	return CAIRO_STATUS_SUCCESS == cairo_surface_write_to_png((cairo_surface_t*)surf_, path.data());
+}
--- src/plot/backend/cairo/KcCairoSurfaceImage.h
+++ src/plot/backend/cairo/KcCairoSurfaceImage.h
@@ -1,11 +1,14 @@
 #pragma once
 #include "KvCairoSurface.h"
+#include <string_view>
 
 
 class KcCairoSurfaceImage : public KvCairoSurface
 {
 public:
 	void* data() const;
+
+	bool saveToFile(const std::string_view& path) const;
 
 protected:
 	void* createSurface_() const override;
